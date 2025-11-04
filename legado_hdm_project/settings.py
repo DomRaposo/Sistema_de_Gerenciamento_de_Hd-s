df8334989f4da083cef6cf199d4ee5f61addfcc7
DomRaposo <felipe_ol@outlook.com>
2025-11-04 14:07:19 -0300

    adicionar cors exceções para o front consumir

--- legado_hdm_project/settings.py
+++ legado_hdm_project/settings.py
@@ -22,6 +22,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+]
+
 
 
 INSTALLED_APPS = [
@@ -34,10 +39,12 @@ INSTALLED_APPS = [
     'rest_framework',         
     'rest_framework_simplejwt',
     'hdm_manager',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
